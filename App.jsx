5c87897e00180dbbb7bb3eb715a4c6a53f48e2ea
Sachin Kumar <skraja012345678@gmail.com>
2025-09-21 10:45:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import About from './components/About'
+import User from './components/User'
+import './App.css'
+
+
+import { createBrowserRouter ,RouterProvider} from 'react-router-dom'
+function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <> <Navbar/> <Home/></>,
+    },
+    {
+      path:"/login",
+      element:<> <Navbar/> <Login/></>
+    },
+    {
+      path:"/about",
+      element:<> <Navbar/> <About/></>
+    },
+    {
+      path:"/user/:username",
+      element:<> <Navbar/> <User/></>
+    },
+
+  ])
+
+  return (
+    <>
+  
+        
+        <RouterProvider router={router}/>
+
+        
+    </>
+  )
+}
+
+export default App
